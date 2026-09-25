2212c1fa5ff371fd7d64fdf62e6ba1c186c773ec
Rayablepy <rayhan11.0808@gmail.com>
2026-09-25 11:39:41 +0800

    Refactored model loading to main

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
 load_dotenv()
+
 CHAT_MODEL_NAME=os.getenv("CHAT_MODEL_NAME")
 ACTUAL_FILE_PATH="./user_data/"
 EMBEDDING_MODEL_NAME=os.getenv("EMBEDDING_MODEL_NAME")
 EMBEDDING_MODEL_CONTEXT=int(os.getenv("EMBEDDING_MODEL_CONTEXT", 512))
 EMBEDDING_MODEL_CHUNK=int(os.getenv("EMBEDDING_MODEL_CHUNK", 64))
+
+tokenizer = AutoTokenizer.from_pretrained(CHAT_MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(CHAT_MODEL_NAME)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+hf = HuggingFacePipeline(pipeline=pipe)
+model=ChatHuggingFace(llm=hf)
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 
-from config import CHAT_MODEL_NAME
 from loader import query_data
 from langchain.agents import create_agent
-from langchain_huggingface import ChatHuggingFace
-from langchain_huggingface.llms import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-
 tools = [query_data]
-
-model_id = CHAT_MODEL_NAME
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-hf = HuggingFacePipeline(pipeline=pipe)
-
-model=ChatHuggingFace(llm=hf)
 
 agent = create_agent(
     model=model,
