5e32563fbcb5671be3810347645d03b4f277eb6c
Rayablepy <rayhan11.0808@gmail.com>
2026-09-26 19:23:15 +0800

    attempted fix for incompatible async hf pipeline

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from langchain_huggingface import ChatHuggingFace
 from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-
+import asyncio
+from functools import partial
 load_dotenv()
 
 CHAT_MODEL_NAME=os.getenv("CHAT_MODEL_NAME")
@@ -17,4 +18,12 @@ tokenizer = AutoTokenizer.from_pretrained(CHAT_MODEL_NAME,cache_dir=MODEL_PATH)
 model = AutoModelForCausalLM.from_pretrained(CHAT_MODEL_NAME,cache_dir=MODEL_PATH)
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 hf = HuggingFacePipeline(pipeline=pipe)
-CHAT_MODEL=ChatHuggingFace(llm=hf)
+
+class AsyncHFAdapter:
+    def __init__(self,model: HuggingFacePipeline):
+        self.model=model
+    async def ainvoke(self,*args,**kwargs):
+        return await asyncio.get_running_loop().run_in_executor(None,partial(self.model.invoke,*args,**kwargs))
+
+async_hf=AsyncHFAdapter(hf)
+CHAT_MODEL=ChatHuggingFace(llm=async_hf)
